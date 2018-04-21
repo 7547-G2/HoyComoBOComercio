d23a7653c2bbf47433deb0e6dc9078b70a711ab0
Javier <javschulz@gmail.com>
2018-04-21 02:35:16 -0300

    Pagination added

--- src/app/comercio/platos/platoController.js
+++ src/app/comercio/platos/platoController.js
@@ -10,6 +10,24 @@
     
     vm.dishes = [];
     
+    $scope.viewby = 10;
+    $scope.totalItems = 0;
+    $scope.currentPage = 1;
+    $scope.itemsPerPage = $scope.viewby;
+    $scope.maxSize = 5; //Number of pager buttons to show
+  
+    $scope.setPage = function (pageNo) {
+      $scope.currentPage = pageNo;
+    };
+  
+    $scope.pageChanged = function() {
+      console.log('Page changed to: ' + $scope.currentPage);
+    };
+  
+  $scope.setItemsPerPage = function(num) {
+    $scope.itemsPerPage = num;
+    $scope.currentPage = 1; //reset to first page
+  }
     
     vm.new_plato = {
       description: null,
@@ -29,6 +47,7 @@
         .then(function (result) {
           if (result.data !== null) {
             result.data.forEach(function (value) {vm.dishes.push(value)});
+            $scope.totalItems = vm.dishes.length;
           }
         })
         .catch(function (err) {
